c4e93cbbebb817d578f281591631b12a3f3dfded
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-01 23:52:02 +0600

    feat: Create TextEditor page with metadata
    - Added main page for the TextEditor component in Next.js.
    - Defined metadata: title, description, and keywords for SEO.
    - Integrated TextEditor with a centered layout for better user experience.
    
    This commit sets up the TextEditor page for user interactions.

---
+++ app/text-editor/page.tsx
@@ -0,0 +1,24 @@
+// app/text-editor/page.tsx
+
+import { Metadata } from 'next';
+import TextEditor from '@/components/text-editor/editor';
+
+export const metadata: Metadata = {
+    title: 'Simple Online Text Editor',
+    description: 'Use this simple online text editor to create, edit, and save .txt and .rtf files directly from your browser. Print your documents as PDFs effortlessly.',
+    keywords: 'online text editor, create text files, edit .txt, edit .rtf, Next.js text editor, print PDF, web app',
+};
+
+export default function QrCodeLayout() {
+
+    return (
+        <main>
+            <h1 className='sr-only'>{metadata.title as string}</h1>
+            <h2 className='sr-only'>{metadata.description as string}</h2>
+
+            <div className="min-h-screen flex items-center justify-center">
+                <TextEditor />
+            </div>
+        </main>
+    );
+}
