5097ef20e15d2b828b20f8d5cd9046dc5fb1fd8e
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-02 02:19:04 +0600

    feat: define markdown homepage with metadata

---
+++ app/edit-and-live-preview-markdown/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from 'next';
+import MarkdownPlayground from '@/components/markdown/MarkdownPlayground';
+
+export const metadata: Metadata = {
+    title: 'Online Markdown Editor & Live Previewer',
+    description: 'Edit, create, and preview Markdown files in real-time with this intuitive online Markdown editor. Effortlessly print your documents as PDFs.',
+    keywords: 'Markdown editor, live Markdown preview, online Markdown tool, create Markdown files, edit Markdown documents, PDF export, Next.js Markdown editor, Markdown playground, web-based Markdown editor',
+};
+
+export default function MarkdownHomepage() {
+    return (
+        <main>
+            <h1 className='sr-only'>{metadata.title as string}</h1>
+            <h2 className='sr-only'>{metadata.description as string}</h2>
+
+            <MarkdownPlayground />
+        </main>
+    );
+}
