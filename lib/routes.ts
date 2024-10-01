94106c3e54c83bb9d80a6cfebd68a25c0c57b771
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-01 19:59:08 +0600

    lib: define basic internal routes

---
+++ lib/routes.ts
@@ -0,0 +1,11 @@
+export const HOME_ROUTE = '/';
+export const QR_ROUTE = '/qr-code';
+export const QR_GENERATE_ROUTE = '/qr-code/generate';
+export const QR_READ_ROUTE = '/qr-code/read';
+export const BAR_CODE_ROUTE = '/bar-code';
+export const BAR_CODE_GENERATE_ROUTE = '/bar-code/generate';
+export const BAR_CODE_READ_ROUTE = '/bar-code/read';
+export const COLOR_ROUTE = '/color';
+export const TEXT_EDITOR_ROUTE = '/text-editor';
+export const MARKDOWN_PREVIEWER_ROUTE = '/markdown-previewer';
+
