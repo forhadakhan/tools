eedb5261e6aa7d83dc52efbc853f46750aeecf28
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-02 02:13:20 +0600

    feat: define markdown playgroud with both editor & previewer

---
+++ components/markdown/MarkdownPlayground.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from 'react';
+import MarkdownEditor from '@/components/markdown/MarkdownEditor';
+import MarkdownPreview from '@/components/markdown/MarkdownPreview';
+
+export default function MarkdownPlayground() {
+    const [markdown, setMarkdown] = useState('')
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4">
+            <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+            <MarkdownPreview markdown={markdown} />
+        </div>
+    );
+}
