3b6508ede635b99b82cedb7ae037ba70e5a059b9
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-01 23:48:03 +0600

    ui: define a reusable file input

---
+++ components/ui/FileInput.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import React, { useRef } from 'react';
+import { Button } from "@/components/ui/button";
+import { FolderOpenIcon } from 'lucide-react';
+
+interface FileInputProps {
+    onFileSelect: (file: File) => void; // Callback function triggered when a file is selected.
+    accept?: string;    // The accepted file types for the input. Defaults to all types.
+    className?: string; // Additional class names for the button.
+    label?: string;     // The label to display on the button.
+    size?: "default" | "sm" | "lg" | "icon" | null | undefined; // The size of the button.
+    hideText?: boolean; // If true, the button will hide the text label and only show the icon.
+}
+
+/**
+ * FileInput component allows users to select a file through a button.
+ *
+ * @param {FileInputProps} props - The props for the component.
+ * @returns {JSX.Element} The rendered FileInput component.
+ */
+export function FileInput({ onFileSelect, accept, className, label, size, hideText }: FileInputProps) {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleFileOpen = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            onFileSelect(file);
+        }
+    };
+
+    return (
+        <>
+            <input
+                type="file"
+                accept={accept || '*'}
+                onChange={handleFileOpen}
+                ref={fileInputRef}
+                className="hidden"
+            />
+            <Button size={size || 'default'} onClick={() => fileInputRef.current?.click()} className={className}>
+                <FolderOpenIcon className={cn("h-4 w-4", !hideText && "mr-2")} />
+                {hideText ? '' : label || 'Open File'}
+            </Button>
+        </>
+    );
+}
+
+export default FileInput;
