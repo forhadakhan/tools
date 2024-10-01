b80da2b2fbc544fc663407ad58647527f26fa6d0
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-01 20:04:51 +0600

    feat: add title+icon and app list in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Apps from "@/components/apps";
+import HomeLink from "@/components/HomeLink";
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
 
-      Tools by FORHAD KHAN
+      <HomeLink
+        className="bg-transparent text-gray-950 border border-transparent hover:border-black my-4"
+        textClassName="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-4"
+      />
+
+      <Apps />
 
     </main>
   );
