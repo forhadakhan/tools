3c27346cf94bce23e11f322be0c4c584078da3c9
Forhad Khan <67508944+forhadakhan@users.noreply.github.com>
2024-10-05 04:07:37 +0600

    lib: updata sample markdown data

--- components/markdown/SampleMarkdown.ts
+++ components/markdown/SampleMarkdown.ts
@@ -1,10 +1,10 @@
 
-const SampleMarkdown = `# Welcome to my React Markdown Previewer!
+const SampleMarkdown = `# Welcome to the Modern Markdown Tool.
 
-## This is a sub-heading...
-### And here's some other cool stuff:
+## Edit and preview your content in real-time! You can also easily export your work as a PDF.
+### Get a clear visualization of what the code conveys.
 
-Heres some code, \`<div></div>\`, between 2 backticks.
+Here's some inline code: \`<div></div>\`, enclosed between two backticks.
 
 \`\`\`
 // this is multi-line code:
@@ -16,30 +16,31 @@ function anotherExample(firstLine, lastLine) {
 }
 \`\`\`
 
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
+You can format text as **bold**, _italic_, or **_both!_**.
+If you need to strike through something, feel free to use ~~strikethrough~~.
+You can use \`html\` for additional formatting, such as <u>underlined text</u>.
 
-There's also [links](https://www.freecodecamp.org), and
+There's also [links](https://tools.forhadakhan.com), and
 > Block Quotes!
 
-And if you want to get really crazy, even tables:
+And if you want to explore further, you can even create tables:
 
-| Wild Header | Crazy Header | Another Header? |
-| ------------ | ------------- | ------------- |
-| Your content can | be here, and it | can be here... |
-| And here. | Okay. | I think we get it. |
+| Wild Header        | Crazy Header         | Another Header?      |
+|---------------------|---------------------|-----------------------|
+| Content can be placed here. | Additional content can be here. | This space can also be utilized. |
+| More content goes here.     | Understood.         | I believe we have covered everything. |
 
-- And of course there are lists.
+- Of course, there are lists.
   - Some are bulleted.
-     - With different indentation levels.
-         - That look like this.
+     - They can have different indentation levels.
+         - Like this.
 
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
+1. There are also numbered lists.  
+1. You can use just "1" for all items if you prefer!  
+1. Last but not least, let's not forget about embedded images:  
 
-![markdown Logo](https://img.crx4chrome.com/b6/51/1e/ehnpcjdjgnnjibdeacicbkhgagkdjgfn-logo.png)`;
+![markdown Logo](https://img.crx4chrome.com/b6/51/1e/ehnpcjdjgnnjibdeacicbkhgagkdjgfn-logo.png)
+
+![Markdown Tool Cover](/banner-image.jpeg)`;
 
 export default SampleMarkdown;
